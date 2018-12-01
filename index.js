5ac478d1a77de18eaa06b15cda9f479f1753eef3
Idonava <idonava@gmail.com>
2018-12-01 15:20:19 +0200

    check

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT =    process.env.PORT || 5000;
 app.get('/', (req, res) => {
-    res.send({ hi: 'there' });
+    res.send({ bye: 'there' });
 })
 app.listen(PORT);
