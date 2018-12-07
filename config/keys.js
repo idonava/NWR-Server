033ece1b44c2f8e08fd9bdb81f81465128fbaa59
Idonava <idonava@gmail.com>
2018-12-07 14:00:12 +0200

    module typo

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,8 @@
 if (process.env.NODE_ENV === 'production') {
     //we are in production - return the prod set of keys.    
-    moudle.exports = require('./prod');
+    module.exports = require('./prod');
 }
 else {
     //we are in development - return the dev keys!
-    moudle.exports = require('./dev');
+    module.exports = require('./dev');
 }
