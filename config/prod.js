a952f5b1ad4e87ca24280c35df8e6e8e5f557b16
Idonava <idonava@gmail.com>
2018-12-07 13:45:38 +0200

    test2

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 // production keys
-module.exports = () => {
+module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY   
+    cookieKey: process.env.COOKIE_KEY
 };
