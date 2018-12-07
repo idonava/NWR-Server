07c4aa689257a1a492c8138d5147c92d92271db9
Idonava <idonava@gmail.com>
2018-12-07 13:25:15 +0200

    change node dev to node env

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,6 @@
-if (process.env.NODE_DEV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     //we are in production - return the prod set of keys.    
     moudle.exports = require('./prod');
-
 }
 else {
     //we are in development - return the dev keys!
--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 // production keys
-module.exports = {
+module.exports = () => {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
