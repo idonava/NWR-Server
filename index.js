435d9fe33bd2c2040d3428ce04996f79b598b4ca
Idonava <idonava@gmail.com>
2018-12-07 11:53:33 +0200

    user models update

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys.js');
-require('./models/User');
+const User = require ('./models/User')
+
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
