640eaf93dbebf0ab4357f78210296b7b42e158ff
Idonava <idonava@gmail.com>
2018-12-21 13:04:10 +0200

    end of lecture 87 & section 7

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,14 +5,20 @@ module.exports = (app) => {
         scope: ['profile', 'email']
     }))
 
-    app.get('/auth/google/callback', passport.authenticate('google'))
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
+        }
+    );
 
     app.get('/api/logout', (req, res) => {
         req.logout(req.user);
-        res.send(req.user);
+        res.redirect('/');
     });
     app.get(
         '/api/current-user', (req, res) => {
-        res.send(req.user);  // put everything in the cookie
-      });
+            res.send(req.user);  // put everything in the cookie
+        });
 };
