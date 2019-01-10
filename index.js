443ddf4580b49cd985b2f47a0899b7d71229a8c2
Idonava <idonava@gmail.com>
2019-01-10 20:49:08 +0200

    fix

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ require("./routes/billingRoutes")(app);
 if (process.env.NODE_ENV === 'production') {
     // Express will serve up produdation assets
     // Like our main.js file/main.css.
-    app.use(express.static('clinet/build'));
+    app.use(express.static("client/build"));
     // Express will serve up our index.html file
     // if it doesn't recognize the route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
 
